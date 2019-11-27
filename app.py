d83f0e6ef0c242255fe0596508c4cc2a136c5ef1
Ubuntu <ubuntu@ip-172-31-95-44.ec2.internal>
2019-11-27 11:08:38 +0000

    Succcesfully connected Flask to MongoDB Atlas - Need to input PASSWORD

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'ocean_dictionary'
+app.config["MONGO_URI"] = 'mongodb+srv://RobSimons1:********@myfirstcluster-yyi3w.mongodb.net/ocean_dictionary?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_words')
+def get_words():
+    return render_template("words.html", words=mongo.db.words.find())
 
 
 if __name__ == '__main__':
