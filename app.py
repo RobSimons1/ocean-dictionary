2d2fd07ffb628b50294e2927c97a7d3cab31dac1
RobSimons1 <rob.simons79@gmail.com>
2019-12-08 11:32:19 +0000

    editcategory.html file added and fully functional

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def add_word():
     return render_template('addword.html',
     categories=mongo.db.categories.find())  
 
-   
+  
 @app.route('/insert_word', methods=['POST'])
 def insert_word():
     words = mongo.db.words
@@ -57,7 +57,22 @@ def delete_word(word_id):
 @app.route('/get_categories')
 def get_categories():
     return render_template('categories.html',
-                           categories=mongo.db.categories.find())          
+                           categories=mongo.db.categories.find())   
+
+
+@app.route('/edit_category/<category_id>')
+def edit_category(category_id):
+    return render_template('editcategory.html',
+                           category=mongo.db.categories.find_one(
+                           {'_id': ObjectId(category_id)}))
+
+
+@app.route('/update_category/<category_id>', methods=['POST'])
+def update_category(category_id):
+    mongo.db.categories.update(
+        {'_id': ObjectId(category_id)},
+        {'category_name': request.form.get('category_name')})
+    return redirect(url_for('get_categories'))                                 
                             
 if __name__ == "__main__":
     app.run(host=os.getenv("IP"),
